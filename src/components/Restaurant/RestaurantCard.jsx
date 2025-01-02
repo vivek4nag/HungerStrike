c6ee88888e2e2c6a268bb6ed3552cb5ba12eb8af
viek4nag <vivek4nag@gmail.com>
2025-01-02 18:51:29 +0530

    cards edited

--- src/components/Restaurant/RestaurantCard.jsx
+++ src/components/Restaurant/RestaurantCard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faLocationDot, faUtensils } from "@fortawesome/free-solid-svg-icons";
 
 const RestaurantCard = ({ name, rating, address, outcode, postcode, type_of_food }) => {
 
@@ -10,38 +10,36 @@ const RestaurantCard = ({ name, rating, address, outcode, postcode, type_of_food
         //     stars.push(i <= rating ? <faStar key={i} className="text-yellow-500" /> : <faStar key={i} className="text-gray-300" />);
         // }
         // console.log(stars);
-        
+
         // return stars;
 
-        for (let i = 1; i <= Math.floor(rating); i++){
-            return <FontAwesomeIcon icon={faStar} key ={i} className = "text-yellow-500"/>
+        for (let i = 1; i <= Math.floor(rating); i++) {
+            return <FontAwesomeIcon icon={faStar} key={i} className="text-yellow-500" />
         }
     };
 
 
     return (
         <div className="p-4 bg-slate-100 rounded-md shadow-md w-80 text-gray-800 hover:scale-95 cursor-pointer transition-all duration-200 ">
-            
+
             <div className="flex justify-between items-center mb-2">
                 <h2 className="text-xl font-bold">{name}</h2>
                 {/* <div className="flex">{renderStars(rating)}</div> */}
             </div>
 
             <div className="mb-2">
-                <p className="text-sm">{address}</p>
+                <p className="text-sm"><span><FontAwesomeIcon icon={faLocationDot}/>{" "}</span>{address}</p>
             </div>
 
-            <div className="mb-2">
-                <p className="text-sm">
-                    <span className="font-medium">Outcode:</span> {outcode}
+            <div className="mb-2 flex">
+                <p className="text-sm text-gray-500">
+                    {outcode} {postcode}
                 </p>
-                <p className="text-sm">
-                    <span className="font-medium">Postcode:</span> {postcode}
-                </p>
+
             </div>
 
-            <div className="text-sm italic text-gray-600">
-                <p>{type_of_food}</p>
+            <div className="text-md font-medium italic text-green-600">
+                <p><span><FontAwesomeIcon icon={faUtensils} /> {" "}</span>  {type_of_food}</p>
             </div>
         </div>
     );
